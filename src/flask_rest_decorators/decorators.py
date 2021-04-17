4e9095126090e17fea61df06641e7d532ecd6a1a
Márcio Oliveira <marciomjmo@gmail.com>
2021-04-16 22:26:44 -0300

    Added documentation to decorators.

--- src/flask_rest_decorators/decorators.py
+++ src/flask_rest_decorators/decorators.py
@@ -1,19 +1,50 @@
 import flask
 
 def controller(endpoint):
+    """
+    Turns the decorated class into a rest controller.
+
+    This methods creates a class variable '__blueprint__' on the decorated class 
+    and then adds url rules for any methods of the class that are decorated 
+    with @get, @post, @put or @delete. The '__blueprint__' variable is an 
+    instance of flask.Blueprint. It also creates a method 'register_routes' on 
+    the decorated class for registering the blueprint on the app.
+
+    Args:
+        endpoint (str): The controller endpoint.
+
+    Returns:
+        The original class augmented with a '__blueprint__' variable and a 'register_routes' method.
+    """
     def add_blueprint(cls):
-        cls.blueprint = flask.Blueprint( cls.__name__, cls.__module__, url_prefix = endpoint )
+        cls.__blueprint__ = flask.Blueprint( cls.__name__, cls.__module__, url_prefix = endpoint )
+        cls.register_routes = lambda app: app.register_blueprint(cls.__blueprint__)
         
-        methods = [getattr(cls, m) for m in dir(cls) if not m.startswith('__') ]
+        methods = [getattr(cls, m) for m in dir(cls)]
         for m in methods:
-            if ( hasattr(m,"__rest__") ):
-                cls.blueprint.add_url_rule( m.__rest__["rule"], m.__name__, m, **m.__rest__["options"])
+            if ( hasattr(m,"__rest_metainfo__") ):
+                cls.__blueprint__.add_url_rule( m.__rest_metainfo__["rule"], m.__name__, m, **m.__rest_metainfo__["options"])
         return cls
     return add_blueprint
 
 def get(rule, **options):
+    """
+    Maps a GET request for a controller's method.
+
+    This method creates a variable '__rest_metainfo__' on the decorated 
+    method with meta-info about the rule and options of the request mapping.
+    This info will be used by @controller decorator when building the class' blueprint.
+
+    Args:
+        rule (str): The url rule that the method handles.
+        options (dict): Extra options. See flask blueprint documentation 
+        for learning more.
+
+    Returns:
+        The original method augmented with a '__rest_metainfo__' variable.
+    """
     def method_wrapper(f):
-        f.__rest__ = {
+        f.__rest_metainfo__ = {
             "rule": rule,
             "options": {
                 **options,
@@ -24,8 +55,23 @@ def get(rule, **options):
     return method_wrapper
 
 def post(rule, **options):
+    """
+    Maps a POST request for a controller's method.
+
+    This method creates a variable '__rest_metainfo__' on the decorated 
+    method with meta-info about the rule and options of the request mapping.
+    This info will be used by @controller decorator when building the class' blueprint.
+
+    Args:
+        rule (str): The url rule that the method handles.
+        options (dict): Extra options. See flask blueprint documentation 
+        for learning more.
+
+    Returns:
+        The original method augmented with a '__rest_metainfo__' variable.
+    """
     def method_wrapper(f):
-        f.__rest__ = {
+        f.__rest_metainfo__ = {
             "rule": rule,
             "options": {
                 **options,
@@ -37,8 +83,23 @@ def post(rule, **options):
 
 
 def put(rule, **options):
+    """
+    Maps a PUT request for a controller's method.
+
+    This method creates a variable '__rest_metainfo__' on the decorated 
+    method with meta-info about the rule and options of the request mapping.
+    This info will be used by @controller decorator when building the class' blueprint.
+
+    Args:
+        rule (str): The url rule that the method handles.
+        options (dict): Extra options. See flask blueprint documentation 
+        for learning more.
+
+    Returns:
+        The original method augmented with a '__rest_metainfo__' variable.
+    """
     def method_wrapper(f):
-        f.__rest__ = {
+        f.__rest_metainfo__ = {
             "rule": rule,
             "options": {
                 **options,
@@ -49,8 +110,23 @@ def put(rule, **options):
     return method_wrapper
 
 def delete(rule, **options):
+    """
+    Maps a DELETE request for a controller's method.
+
+    This method creates a variable '__rest_metainfo__' on the decorated 
+    method with meta-info about the rule and options of the request mapping.
+    This info will be used by @controller decorator when building the class' blueprint.
+
+    Args:
+        rule (str): The url rule that the method handles.
+        options (dict): Extra options. See flask blueprint documentation 
+        for learning more.
+
+    Returns:
+        The original method augmented with a '__rest_metainfo__' variable.
+    """
     def method_wrapper(f):
-        f.__rest__ = {
+        f.__rest_metainfo__ = {
             "rule": rule,
             "options": {
                 **options,
